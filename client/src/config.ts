9ef9a3755b6e394b73d05c46523beda3b911a27e
Michael Worm <michael.worm@essquare.de>
2020-10-07 22:27:05 +0200

    Init

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'wacmyll98c'
+export const apiEndpoint = `https://${apiId}.execute-api.eu-central-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-tr29yf2q.eu.auth0.com',           // Auth0 domain
+  clientId: 'lx9O7MOwmiMxWGIBFP4w27C3EaEerGrK',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
